fd15e918d99aca712a9c712af0795fb7f2f208c6
T-sosthenese <sosthenetimi94@gmail.com>
2024-01-14 23:58:04 +0300

    Portfolio

---
+++ react-portfolio/src/components/Contact/Contact.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:sosthenetimi94@gmail.com">sosthenetimi94@gmail.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.linkedin.com/in/sosthene-timi-bb134b270/">linkedin.com/Sosthene Timi</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://github.com/T-sosthenese">github.com/T-sosthenese</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
